edda777ef31e39dfe321023d71bc976ceebebfda
aaryankotharii <aaryan.kothari@gmail.com>
2020-02-12 13:15:27 +0530

    functionality complete (almost)

--- BMI/Controller/ViewController.swift
+++ BMI/Controller/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        Utilities.styleFilledButton(signUpButton)
+        Utilities.styleHollowButton(loginButton)
         // Do any additional setup after loading the view.
     }
     
---
+++ BMI/Utilities/Utilities.swift
@@ -0,0 +1,42 @@
+//
+//  Utilities.swift
+//  BMI
+//
+//  Created by Aaryan Kothari on 12/02/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+
+import UIKit
+
+class Utilities {
+    
+    
+static func styleTextField(_ textfield:UITextField) {
+
+    // Create the bottom line
+    let bottomLine = CALayer()
+    bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+    bottomLine.backgroundColor = UIColor.init(red: 215/255, green: 106/255, blue: 138/255, alpha: 1).cgColor
+    textfield.borderStyle = .none
+    // Add the line to the text field
+    textfield.layer.addSublayer(bottomLine)
+     }
+    
+    
+static func styleFilledButton(_ button:UIButton) {
+    // Filled rounded corner style
+    button.backgroundColor = UIColor.white
+    button.layer.cornerRadius = 25.0
+    button.tintColor = UIColor.white
+}
+
+static func styleHollowButton(_ button:UIButton) {
+    // Hollow rounded corner style
+    button.layer.borderWidth = 2
+    button.layer.borderColor = UIColor.white.cgColor
+    button.layer.cornerRadius = 25.0
+    button.tintColor = UIColor.white
+}
+}
+
